4ebe62e03eca98a8ea0de815f66f8f6c5a89bd84
retrevor <75954541+retrevor@users.noreply.github.com>
2022-03-30 20:06:26 +0100

    Basic UI settings to configure swarm node. (#15)
    * Basic UI settings to configure swarm node.

---
+++ lib/Settings/Admin.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * @copyright Copyright (c)
+ *
+ * @author
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace OCA\Files_External_BeeSwarm\Settings;
+
+use OCA\Files_External\Service\BackendService;
+use OCA\Files_External\Service\GlobalStoragesService;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IConfig;
+use OCP\Settings\ISettings;
+
+class Admin implements ISettings {
+
+	/** @var string */
+	protected $appName;
+
+	/** @var IConfig */
+	private $config;
+
+	/** @var GlobalStoragesService */
+	private $globalStoragesService;
+
+	public function __construct($appName,
+								IConfig $config,
+								GlobalStoragesService $globalStoragesService) {
+		$this->appName = $appName;
+		$this->config = $config;
+		$this->globalStoragesService = $globalStoragesService;
+	}
+
+	/**
+	 * @return TemplateResponse
+	 */
+	public function getForm(): TemplateResponse {
+		// Get Mount settings
+		$mounts_json = $this->config->getAppValue($this->appName,"storageconfig","");	//default
+		$mounts = json_decode($mounts_json,  true);
+
+		// Get all valid Beeswarm storages
+		$storageBackends = array_filter($this->globalStoragesService->getStorages(), function ($storageBackend) use ($_) {
+			return $storageBackend->getBackend()->getStorageClass() == '\OCA\Files_External_BeeSwarm\Storage\BeeSwarm';
+		});
+
+		// remove configurations that are no longer in Storages
+		foreach ($mounts as $key => $mount) {
+			$eachmountId = $mount['mount_id'];
+			if (!isset($storageBackends[$eachmountId])) {
+				// remove by key index
+				unset($mounts[$key]);
+			}
+		}
+
+		$mountIds = array_column($mounts, 'mount_id');
+		foreach ($storageBackends as $backend) {
+			$backendId = $backend->getId();
+			$backendName = $backend->getMountPoint();
+			$urlOptions = $backend->getBackendOptions();
+
+			// Is it configured?
+			$key = array_search($backendId, $mountIds);
+			if (!empty($key) || $key === 0) {
+				$mounts[$key]['mount_name'] = $backendName;
+				$batchId = isset($mounts[$key]['batchid']) ? $mounts[$key]['batchid'] : "";
+				$mounts[$key]['batchbalance'] = $this->getBatchBalance($batchId, $urlOptions);
+				$mounts[$key]['chequebalance'] = $this->getChequebookBalance($urlOptions);
+			}
+			else {
+				// not yet configured, so add to array. Default to encryption.
+				$newMounts[] = ['mount_id' => $backendId, 'mount_name' => $backendName, 'encrypt'=> '1'];
+			}
+		}
+		if ($newMounts) {
+			$mounts = array_merge($mounts, $newMounts);
+		}
+
+		$parameters = [
+			'visibilityType' => BackendService::VISIBILITY_ADMIN,
+			'mounts' => json_encode($mounts),
+		];
+		return new TemplateResponse($this->appName, 'admin-settings', $parameters, '');
+	}
+
+	/**
+	 * @return string the section ID, e.g. 'sharing'
+	 */
+	public function getSection(): string {
+		return $this->appName;
+	}
+
+	/**
+	 * @return int whether the form should be rather on the top or bottom of
+	 * the admin section. The forms are arranged in ascending order of the
+	 * priority values. It is required to return a value between 0 and 100.
+	 *
+	 * E.g.: 70
+	 */
+	public function getPriority(): int {
+		return 5;
+	}
+
+	public function getBatchBalance(string $batchId, $urlOptions) {
+		$uri = "/stamps/" . $batchId;
+		$curl = $this->setCurl($uri, $urlOptions);
+
+		$output = curl_exec($curl);
+
+		$response_data = json_decode($output, true);
+
+		curl_close($curl);
+		return isset($response_data["batchTTL"]) ? $response_data['batchTTL'] : null;
+	}
+
+	public function getChequebookBalance($urlOptions) {
+		$uri = "/chequebook/balance";
+		$curl = $this->setCurl($uri, $urlOptions);
+
+		$output = curl_exec($curl);
+
+		$response_data = json_decode($output, true);
+
+		curl_close($curl);
+		return isset($response_data["totalBalance"]) ? $response_data['totalBalance'] : null;
+	}
+
+	/**
+	 * initializes a curl handler
+	 * @return \CurlHandle
+	 */
+	protected function setCurl(string $uri_params, array $urlOptions) : \CurlHandle {
+		$url_endpoint = $urlOptions['ip'] . ":" . $urlOptions['debug_api_port'];
+		$url_endpoint .= $uri_params;
+
+		$curl = curl_init();
+
+		curl_setopt($curl, CURLOPT_URL, $url_endpoint);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+		if (isset($urlOptions['user']) && isset($urlOptions['password'])) {
+
+			$base64EncodedAuth = base64_encode($urlOptions['user'] . ':' . $urlOptions['password']);
+			$header = 'Authorization: Basic ' . $base64EncodedAuth;
+			curl_setopt($curl, CURLOPT_HTTPHEADER, array($header));
+		}
+		return $curl;
+	}
+}
---
+++ templates/admin-settings.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ *
+ * @author
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+style('files_external_beeswarm', 'settings-admin');
+script('files_external_beeswarm', 'settings');
+?>
+<div id="beeswarm" class="section">
+	<h2 class="inlineblock"><?php p($l->t('Bee Swarm Storage')); ?></h2>
+	<a target="_blank" rel="noreferrer" class="icon-info" title="<?php p($l->t('Open documentation'));?>" href="<?php p('https://github.com/MetaProvide/nextcloud-swarm-plugin/'); ?>"></a>
+    <p class="settings-hint"><?php p($l->t('View the current status of the Swarm node(s) configured in \'External Storage\' section of NextCloud.')); ?></p>
+	<?php
+	// Get configured storage mounts from parameters
+	$mounts = json_decode($_['mounts'], true);
+	$mountIds = array_column($mounts, 'mount_id');
+	// Make comma-seperated list of storageIds to use in hidden control in this form.
+	$controlmountIds = implode(",", $mountIds);
+
+	foreach ($mounts as $mount) :
+		$mountId = $mount['mount_id'];
+		$mountName = $mount['mount_name'];
+		$encrypted = $mount['encrypt'];
+		$batchId =  isset($mount['batchid']) ? $mount['batchid'] : "";
+		$batchBalance = isset($mount['batchbalance']) ? $mount['batchbalance'] : "";
+		$chequeBalance = isset($mount['chequebalance']) ? $mount['chequebalance'] : "";
+	?>
+	<div>
+		<label>
+			<h3><b><span id="<?php p($mountId);?>"><?php p($l->t('Swarm node')) ?></b>: <?php p($mountName); ?></span></h3>
+		</label>
+	</div>
+	<div>
+		<label>
+			<span class="label"><?php p($l->t('Allow encryption')) ?></span>
+			<?php
+			if ($encrypted) {
+				$checked = "checked";
+			}?>
+			<span><input id="beeswarm_encrypt_<?php p($mountId); ?>" type="checkbox" <?php p($checked) ?>/></span>
+		</label>
+	</div>
+	<div>
+		<label>
+			<span class="label"><?php p($l->t('Current Batch Id')) ?></span>
+			<span><input id="beeswarm_batchid_<?php p($mountId); ?>" type="text" maxlength="200" class="inputBatch" value="<?php p($batchId)?>" /></span>
+		</label>
+	</div>
+	<div>
+		<label>
+			<span class="label"><?php p($l->t('Batch TTL')) ?></span>
+			<span><input id="beeswarm-bzz_<?php p($mountId); ?>" type="text" placeholder="<?php p($l->t('Batch TTL')); ?>" value="<?php p($batchBalance) ?>" maxlength="20" /><span>
+			<span data-setting="bzz" data-toggle="tooltip" data-original-title="<?php p($l->t('Reset to default')); ?>" class="theme-undo icon icon-history"></span>
+		</label>
+	</div>
+	<div>
+		<label>
+			<span class="label"><?php p($l->t('Chequebook Balance')) ?></span>
+			<input id="beeswarm-chequebalance_<?php p($mountId);?>" type="text" maxlength="20" value="<?php p($chequeBalance) ?>" />
+			<div data-setting="chequebalance_" data-toggle="tooltip" data-original-title="<?php p($l->t('Reset to default')); ?>" class="theme-undo icon icon-history"></div>
+		</label>
+	</div>
+	<?php endforeach;	?>
+	<input id="mountsIds" type="hidden" value='<?php p($controlmountIds);?>'>
+
+	<button id="beeswarm-save-settings"><?php p($l->t("Save")); ?></button>
+</div>
