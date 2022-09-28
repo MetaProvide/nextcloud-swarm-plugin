878a6c48dad510c4d4353b34ac9aca8b8029566c
Magnus Walbeck <mw@mwalbeck.org>
2022-09-28 17:08:30 +0200

    Update tool configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,13 @@ module.exports = {
 	extends: ["@nextcloud", "prettier"],
 	rules: {
 		semi: [2, "always"],
+		"no-console": "off",
+		"vue/first-attribute-linebreak": [
+			"error",
+			{
+				singleline: "ignore",
+				multiline: "below",
+			},
+		],
 	},
 };
