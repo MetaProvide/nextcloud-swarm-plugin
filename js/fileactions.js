13db2580aee7a0e511d02447cb5e3f08ba681ef9
Ron <>
2023-02-15 13:48:27 +0000

    Optimization

--- js/fileactions.js
+++ js/fileactions.js
@@ -20,50 +20,42 @@
  *
  */
 window.addEventListener('DOMContentLoaded', () => {
-	var actionSwarm = {
-		registerMenu: function (mimetype) {
-			OCA.Files.fileActions.registerAction({
-				name: 'EthswarmCopyRef',
-				displayName: t('files_external_ethswarm', 'Copy Swarm reference'),
-				altText: t('files_external_ethswarm', 'Copy Swarm reference to clipboard'),
-				mime: mimetype,
-				permissions: OC.PERMISSION_READ,
-				type: OCA.Files.FileActions.TYPE_DROPDOWN,
-				iconClass: 'icon-clippy',
-				actionHandler: function (filename, context) {
-					if (context.$file && context.fileInfoModel.attributes.mountType != "external") {
-						return;
-					}
-					remoteurl = OC.linkToRemoteBase("dav/files/" + OC.currentUser + context.fileInfoModel.attributes['path'] + "/" + filename);
+	OCA.Files.fileActions.registerAction({
+		name: 'EthswarmCopyRef',
+		displayName: t('files_external_ethswarm', 'Copy Swarm reference'),
+		altText: t('files_external_ethswarm', 'Copy Swarm reference to clipboard'),
+		mime: 'all',
+		permissions: OC.PERMISSION_READ,
+		type: OCA.Files.FileActions.TYPE_DROPDOWN,
+		iconClass: 'icon-clippy',
+		actionHandler: function (filename, context) {
+			if (context.$file && context.fileInfoModel.attributes.mountType != "external") {
+				return;
+			}
+			remoteurl = OC.linkToRemoteBase("dav/files/" + OC.currentUser + context.fileInfoModel.attributes['path'] + "/" + filename);
 
-					$.ajax({
-						type: "PROPFIND",
-						async: "false",
-						url: remoteurl,
-						data: '<?xml version="1.0" encoding="UTF-8"?>' +
-							'<d:propfind xmlns:d="DAV:" xmlns:nc="http://nextcloud.org/ns">' +
-							'<d:prop><nc:ethswarm-fileref/></d:prop>' +
-							'</d:propfind>',
-						success: function (element) {
-							swarmref = element.firstChild.firstChild.lastChild.firstChild.firstChild.textContent;
-							navigator.clipboard.writeText(swarmref).then(
-								() => {
-									/* clipboard successfully set */
-									OC.dialogs.info(t('files_external_ethswarm', 'The following Swarm reference has been copied to the clipboard: ') + swarmref, t('files_external_ethswarm', 'Swarm reference'));
-								},
-								() => {
-									/* clipboard write failed */
-									OC.dialogs.info(t('files_external_ethswarm', 'Unable to write to the clipboard, you can manually copy the Swarm reference below: ' + swarmref), t('files_external_ethswarm', 'Swarm reference'));
-								}
-							);
+			$.ajax({
+				type: "PROPFIND",
+				async: "false",
+				url: remoteurl,
+				data: '<?xml version="1.0" encoding="UTF-8"?>' +
+					'<d:propfind xmlns:d="DAV:" xmlns:nc="http://nextcloud.org/ns">' +
+					'<d:prop><nc:ethswarm-fileref/></d:prop>' +
+					'</d:propfind>',
+				success: function (element) {
+					swarmref = element.firstChild.firstChild.lastChild.firstChild.firstChild.textContent;
+					navigator.clipboard.writeText(swarmref).then(
+						() => {
+							/* clipboard successfully set */
+							OC.dialogs.info(t('files_external_ethswarm', 'The following Swarm reference has been copied to the clipboard: ') + swarmref, t('files_external_ethswarm', 'Swarm reference'));
+						},
+						() => {
+							/* clipboard write failed */
+							OC.dialogs.info(t('files_external_ethswarm', 'Unable to write to the clipboard, you can manually copy the Swarm reference below: ' + swarmref), t('files_external_ethswarm', 'Swarm reference'));
 						}
-					});
+					);
 				}
 			});
-		},
-	}
-	// For initial page load
-	if (OCA?.Files_External?.StatusManager) {
-		OCA.Files_External.StatusManager.getMountPointList(actionSwarm.registerMenu('all'));
-	}
+		}
+	});
 });
