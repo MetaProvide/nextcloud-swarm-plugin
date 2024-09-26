b10a97251c5a7a97be24f19b7a14588492bc6999
Mahyar Iranibazaz <mahiarirani@pm.me>
2024-09-26 11:32:54 +0800

    feat(Util): add Env - add: util to access ENV vars

---
+++ lib/Utils/Env.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace OCA\Files_External_Ethswarm\Utils;
+
+class Env
+{
+	/**
+	 * @param string $name
+	 * @return string
+	 */
+	public static function get(string $name): string
+	{
+		return getenv($name);
+	}
+
+	/**
+	 * @param string $name
+	 * @param string $value
+	 * @return bool
+	 */
+	public static function set(string $name, string $value): bool
+	{
+		return putenv($name . '=' . $value);
+	}
+
+	/**
+	 * @return bool
+	 */
+	public static function isDevelopment(): bool
+	{
+		return self::get('ENV') === 'development';
+	}
+}
