135d0ccea31e18c05c5b9aba58f8654e76589ab7
Mahyar Iranibazaz <mahiarirani@pm.me>
2024-10-16 17:56:00 +0800

    fix(docker): - fix: enable log mode - update: cli dev tools

--- cli/dev/linux/clean.sh
+++ cli/dev/linux/clean.sh
@@ -3,12 +3,7 @@ set -euo pipefail
 
 # stop and remove the containers and volumes
 echo "stopping and removing the containers and volumes";
-docker compose down --volumes --remove-orphans
-
-# remove nextcloud source code
-echo "removing nextcloud source code";
-echo "need sudo password to remove dev-environment directory"
-sudo rm -rf dev-environment
+docker compose --profile dev down --volumes --remove-orphans
 
 # done
 echo "cleaned up the dev environment"
--- cli/dev/windows/install.bat
+++ cli/dev/windows/install.bat
@@ -1,36 +1,9 @@
 @echo off
 REM Install Script for Windows
 
-REM Check if nextcloud.local is in the hosts file
-echo Checking if nextcloud.local is in C:\Windows\System32\drivers\etc\hosts
-findstr /C:"nextcloud.local" C:\Windows\System32\drivers\etc\hosts >nul
-if %errorlevel% neq 0 (
-    echo Adding nextcloud.local to hosts file
-    echo 127.0.0.1 nextcloud.local >> C:\Windows\System32\drivers\etc\hosts
-) else (
-    echo nextcloud.local is already in the hosts file
-)
-
-REM Set up environment variables
-if not exist ".env" (
-    echo Creating .env file
-    copy .env.example .env
-
-    echo Updating environment variables in .env file
-    powershell -Command "(Get-Content .env) -replace 'REDIS_HOST_PASSWORD=', 'REDIS_HOST_PASSWORD=secret' | Set-Content .env"
-    powershell -Command "(Get-Content .env) -replace 'MYSQL_PASSWORD=', 'MYSQL_PASSWORD=secret' | Set-Content .env"
-    powershell -Command "(Get-Content .env) -replace 'MYSQL_ROOT_PASSWORD=', 'MYSQL_ROOT_PASSWORD=rootpassword' | Set-Content .env"
-    powershell -Command "(Get-Content .env) -replace 'NEXTCLOUD_ADMIN_PASSWORD=', 'NEXTCLOUD_ADMIN_PASSWORD=swarmbox' | Set-Content .env"
-    powershell -Command "(Get-Content .env) -replace 'APP_URL=localhost', 'APP_URL=nextcloud.local' | Set-Content .env"
-
-    echo .env file created
-) else (
-    echo .env file already exists
-)
-
 REM Start the Docker containers
 echo Starting the containers
-docker-compose up -d --force-recreate --remove-orphans --build
+docker-compose --profile dev up -d --force-recreate --remove-orphans --build
 
 REM Done
 echo Started the dev environment
