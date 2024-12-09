03fab62899679320209e77017927a44270171a40
Mahyar Iranibazaz <mahiarirani@pm.me>
2024-12-10 01:56:14 +0800

    chore(docker): - remove: old hooks

--- docker/nextcloud/hooks/post-installation/0-enable-debug-mode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-if [ "$ENV" = "dev" ]; then
-	php occ config:system:set log_type --value=debug
-	php occ config:system:set debug_mode --value=1
-fi
--- docker/nextcloud/hooks/post-installation/2-install-plugin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-php occ app:enable files_external
-php occ app:enable files_external_ethswarm
