fed07462db224b42651fc94a39f707683e963b55
Mahyar Iranibazaz <mahiarirani@pm.me>
2025-01-06 22:19:08 +0800

    bugfix(mkdir): create folder issue (#99)
    - add: missing import
    - fix: typo in entity name

--- lib/Db/SwarmFileMapper.php
+++ lib/Db/SwarmFileMapper.php
@@ -98,7 +98,7 @@ class SwarmFileMapper extends QBMapper {
 	public function createDirectory(string $path, int $storage, string $token): SwarmFile {
 		$swarm = new SwarmFile();
 		$swarm->setName($path);
-		$swarm->setMimeType(OC::$server->get(IMimeTypeLoader::class)->getId('httpd/unix-directory'));
+		$swarm->setMimetype(OC::$server->get(IMimeTypeLoader::class)->getId('httpd/unix-directory'));
 		$swarm->setSize(1);
 		$swarm->setStorageMtime(time());
 		$swarm->setStorage($storage);
