e69f67b3c944320bb193e2748000cae5a5cd0d34
Mahyar Iranibazaz <mahiarirani@pm.me>
2025-03-15 03:29:52 +0800

    Feature/docker stage (#121)
    * feat(docker): remove profile from dev
    - update: keep main docker compose to dev
    
    * feat(docker): add new stage docker compose
    
    * chore(docker): remove extra hosts for stage

--- cli/dev/windows/start.bat
+++ cli/dev/windows/start.bat
@@ -3,7 +3,7 @@ REM Install Script for Windows
 
 REM Start the Docker containers
 echo Starting the containers
-docker-compose --profile dev up -d --force-recreate --remove-orphans --build
+docker-compose up -d --force-recreate --remove-orphans --build
 
 REM Done
 echo Started the dev environment
