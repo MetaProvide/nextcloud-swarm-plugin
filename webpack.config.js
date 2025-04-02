4e4feaf2441c684011949215450dc786a4fbeb6b
Mahyar Iranibazaz <mahiarirani@pm.me>
2025-04-02 22:09:13 +0800

    fix(webpack): include the modules in the resolve

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
 const path = require('path');
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
-// Add TypeScript handling
 webpackConfig.module.rules.push({
 	test: /\.tsx?$/,
 	loader: 'ts-loader',
 	options: {
-	appendTsSuffixTo: [/\.vue$/],
-	transpileOnly: true
+		appendTsSuffixTo: [/\.vue$/],
+		transpileOnly: true
 	}
 });
-// Add .ts and .tsx to resolved extensions
-// webpackConfig.resolve.extensions.push('.*','.ts', '.js', '.vue', '.*', '.tsx');
-webpackConfig.resolve.extensions.push('.ts', '.tsx');
 
-webpackConfig.entry['fileactions'] = path.join(__dirname, 'src', 'fileactions.js');
-webpackConfig.entry['newfilemenu'] = path.join(__dirname, 'src', 'newfilemenu.js');
-webpackConfig.entry['feedbackform'] = path.join(__dirname, 'src', 'feedbackform.js');
-webpackConfig.entry['renamingEthswarmStore']= path.join(__dirname, 'src/store', 'renamingEthswarm.ts');
+webpackConfig.resolve = {
+	modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
+	extensions: ['.js', '.jsx', '.ts', '.tsx'],
+}
+
+const entryPoints = {
+	'fileactions': 'src/fileactions.js',
+	'newfilemenu': 'src/newfilemenu.js',
+	'feedbackform': 'src/feedbackform.js',
+	'renamingEthswarmStore': 'src/store/renamingEthswarm.ts'
+};
+
+Object.entries(entryPoints).forEach(([name, file]) => {
+	webpackConfig.entry[name] = path.join(__dirname, file);
+});
+
 module.exports = webpackConfig
