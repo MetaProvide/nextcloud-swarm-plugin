9fd3d127f48d0f0ce264461e029914d2184210c9
Mahyar Iranibazaz <mahiarirani@pm.me>
2025-04-28 13:30:30 +0800

    feat(installation): auto install file external app - update: extract extended providers from app - add: enableFilesExternalApp systme - refactor: app base code and injections - remove: unused routes - fix: docs duplicate line

---
+++ lib/AppInfo/ExternalStorage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace OCA\Files_External_Ethswarm\AppInfo;
+
+use OCA\Files_External\Lib\Auth\AuthMechanism;
+use OCA\Files_External\Lib\Backend\Backend;
+use OCA\Files_External\Lib\Config\IAuthMechanismProvider;
+use OCA\Files_External\Lib\Config\IBackendProvider;
+use OCA\Files_External\Service\BackendService;
+use OCA\Files_External_Ethswarm\Auth\AccessKey;
+use OCA\Files_External_Ethswarm\Backend\BeeSwarm;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use Psr\Container\ContainerInterface;
+
+class ExternalStorage implements IBackendProvider, IAuthMechanismProvider
+{
+    public function __construct(protected ContainerInterface $container, protected IBootContext $context)
+    {
+        $context->injectFn(function (BackendService $backendService) {
+            $backendService->registerBackendProvider($this);
+            $backendService->registerAuthMechanismProvider($this);
+        });
+    }
+
+    /**
+     * @return Backend[]
+     */
+    public function getBackends(): array
+    {
+        return [$this->container->get(BeeSwarm::class)];
+    }
+
+    /**
+     * @return AuthMechanism[]
+     */
+    public function getAuthMechanisms(): array
+    {
+        return [$this->container->get(AccessKey::class)];
+    }
+}
