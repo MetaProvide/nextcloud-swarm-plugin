b98a0baa3b0453f2540319a23dcc1bd4cac78c10
Mahyar Iranibazaz <mahiarirani@pm.me>
2025-04-28 13:22:15 +0800

    feat(frontend): update packages - update: webpack, eslint and stylelint - update: nextcloud packages - remove: unused packages - add: open-feature web and flags packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
-const path = require('path');
-const webpackConfig = require('@nextcloud/webpack-vue-config')
-
-webpackConfig.entry['fileactions'] = path.join(__dirname, 'src', 'fileactions.js');
-webpackConfig.entry['newfilemenu'] = path.join(__dirname, 'src', 'newfilemenu.js');
-webpackConfig.entry['feedbackform'] = path.join(__dirname, 'src', 'feedbackform.js');
-module.exports = webpackConfig
+const path = require("path");
+const webpackConfig = require("@nextcloud/webpack-vue-config");
+webpackConfig.resolve.alias = {
+	...webpackConfig.resolve.alias,
+	"@": path.resolve(__dirname, "src"),
+};
+webpackConfig.entry = {
+	...webpackConfig.entry,
+	app: path.join(__dirname, "src", "app.js"),
+};
+module.exports = webpackConfig;
