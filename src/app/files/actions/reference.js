7b0cd53db8f12eb270bd5063f6b6d8061733dc96
Mahyar Iranibazaz <mahiarirani@pm.me>
2025-04-29 16:26:05 +0800

    feat(ref): use dialogs for view swarm ref - update: add button with clipboard icon

--- src/app/files/actions/reference.js
+++ src/app/files/actions/reference.js
@@ -1,6 +1,7 @@
-import { FileAction, FileType, registerFileAction } from "@nextcloud/files";
-import { showInfo, showSuccess, showWarning } from "@nextcloud/dialogs";
+import { FileAction, registerFileAction } from "@nextcloud/files";
+import { Dialog, showSuccess, showWarning } from "@nextcloud/dialogs";
 import SwarmSvg from "@/../img/swarm-logo.svg";
+import ClipboardSvg from "@material-design-icons/svg/filled/content_paste.svg";
 import SvgHelper from "@/util/SvgHelper";
 import FilesHelper from "@/util/FilesHelper";
 
@@ -34,62 +35,48 @@ registerFileAction(
 
 		async exec(node, view) {
 			const swarmref = FilesHelper.getSwarmRef(node);
-			if (node.type === FileType.Folder) {
+			if (FilesHelper.isFolder(node)) {
 				showWarning(
 					t(
 						"files_external_ethswarm",
 						"Folder structure is not yet supported on Swarm. This folder is only available on Nextcloud, although all files within it are accessible on Swarm."
 					),
-					t("files_external_ethswarm", "Swarm reference")
+					t("files_external_ethswarm", "Swarm Reference")
 				);
 				return;
 			}
 
-			showInfo(
-				`
-			<div style="margin: 1rem 0; width: 35rem;" data-swram-ref="${swarmref}">
-				<img src="${SwarmSvg}" alt="Swarm" style="height: 20px; vertical-align: middle;">
-				<span>${t(
-					"files_external_ethswarm",
-					"Click on this message to copy the Swarm Reference into your clipboard"
-				)}</span>
-				<pre style="overflow-x: scroll; max-width: 100%;">${swarmref}</pre>
-			</div>`,
+			await new Dialog("View Swarm Reference", swarmref, [
 				{
-					isHTML: true,
-					onClick: () => {
-						document
-							.querySelector(`div[data-swram-ref="${swarmref}"]`)
-							.parentElement.remove();
+					label: t("files_external_ethswarm", "Copy to Clipboard"),
+					type: "secondary",
+					icon: SvgHelper.convert(ClipboardSvg),
+					callback: () =>
 						navigator.clipboard.writeText(swarmref).then(
-							() => {
+							() =>
 								showSuccess(
 									t(
 										"files_external_ethswarm",
 										"The Swarm reference has been copied to your clipboard"
 									)
-								);
-							},
-							() => {
+								),
+							() =>
 								showWarning(
 									`
-								<div style="margin: 1rem 0; width: 35rem;">
-									<span>${t(
-										"files_external_ethswarm",
-										"Unable to write the Swarm Reference into your clipboard. Copy it manually"
-									)}</span>
-									<pre style="overflow-x: scroll; max-width: 100%;">${swarmref}</pre>
-								</div>
-							`,
+											<div style="margin: 1rem 0; width: 35rem;">
+												<span>${t(
+													"files_external_ethswarm",
+													"Unable to write the Swarm Reference into your clipboard. Copy it manually"
+												)}</span>
+											</div>
+										`,
 									{
 										isHTML: true,
 									}
-								);
-							}
-						);
-					},
-				}
-			);
+								)
+						),
+				},
+			]).show();
 		},
 	})
 );
