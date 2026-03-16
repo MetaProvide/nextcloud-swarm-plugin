e495150b964161bc0385e6b5bd9af04ea42af515
frozenbanana <3958329+frozenbanana@users.noreply.github.com>
2026-03-16 13:02:05 +0000

    chore: fix code style

--- lib/Auth/AccessKey.php
+++ lib/Auth/AccessKey.php
@@ -28,13 +28,11 @@ use OCP\IL10N;
 /**
  * Basic Http Authentication.
  */
-class AccessKey extends AuthMechanism
-{
+class AccessKey extends AuthMechanism {
 	/** @const string */
 	public const SCHEME = 'access_key';
 
-	public function __construct(IL10N $l)
-	{
+	public function __construct(IL10N $l) {
 		$this
 			->setIdentifier('access:key')
 			->setScheme(self::SCHEME)
